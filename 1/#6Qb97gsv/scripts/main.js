19298f8dc1032a98d0970feab9313bb4a29045d7
Yurii <131679817+havr1985@users.noreply.github.com>
2024-11-04 09:52:13 +0200

    end first task

---
+++ 1/#6Qb97gsv/scripts/main.js
@@ -0,0 +1,7 @@
+const firstName = 'Юрій';
+const middleName = 'Михайлович';
+const lastName = 'Гаврик';
+
+const person = `${firstName} ${middleName} ${lastName}`;
+
+console.log(person)
---
+++ 1/#ruUtWDUI/scripts/main.js
@@ -0,0 +1,11 @@
+let firstName = 'Юрій';
+let middleName = 'Михайлович';
+let age = 38;
+
+prompt(firstName);
+prompt(middleName);
+prompt(age);
+
+console.log(firstName);
+console.log(middleName);
+console.log(age);
